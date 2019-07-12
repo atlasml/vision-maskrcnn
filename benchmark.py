20f824f80789115574555e2123955f05f5802f7a
Ross Taylor <rj-taylor@live.co.uk>
2019-07-12 13:43:19 +0100

    Update benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,7 @@
-import torchvisionfrom sotabench.object_detection import COCO
+import torchvision
+from sotabench.object_detection import COCO
 
 model = torchvision.models.detection.__dict__['maskrcnn_resnet50_fpn'](num_classes=91, pretrained=True)
-test_results = COCO.benchmark(batch_size=8, model=model)
+test_results = COCO.benchmark(batch_size=8, 
+                              model=model,
+                              paper_model_name='Mask R-CNN ResNet-50 FPN',)
